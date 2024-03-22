2b38f33c2d815859e7617750fd37497d5487f514
toriruban <156658393+toriruban@users.noreply.github.com>
2024-03-22 12:45:37 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,10 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits){
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
     const totalPrice = quantity * pricePerDroid;
 
-    if(totalPrice > customerCredits){
-        return('Insufficient funds!')
-    }else{
-        return(`You ordered ${quantity} droids worth ${totalPrice} credits!`)
+    if (totalPrice > customerCredits) {
+        return 'Insufficient funds!';
+    } else {
+        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
     }
 }
 
